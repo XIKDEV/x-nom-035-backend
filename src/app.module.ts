ce12acaab648befc997ceaf5b0d827a40db015fd
Alderick Hernán Rios Contreras <93097571+XIKDEV@users.noreply.github.com>
2024-07-18 19:47:39 -0700

    chore: Update eslint.config.mjs to use 'flow' parser for prettier

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 
 import { UserModule } from './modules';
 import { AuthModule } from './providers';
+import { CatalogsModule } from './catalogs/catalogs/catalogs.module';
 import { ModulesModule } from './catalogs/modules/modules.module';
 import { PermissionsModule } from './catalogs/permissions/permissions.module';
 import { RolesModule } from './catalogs/roles/roles.module';
+import { StatesModule } from './catalogs/states/states.module';
 import { TypeTestModule } from './catalogs/type-test/type-test.module';
 import { TypesModuleModule } from './catalogs/types-module/types-module.module';
-import { StatesModule } from './catalogs/states/states.module';
-import { CatalogsModule } from './catalogs/catalogs/catalogs.module';
 
 @Module({
   imports: [
