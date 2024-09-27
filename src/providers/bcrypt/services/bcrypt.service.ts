61dd781099577be9ef08d36b691bd7dfdb796c7c
Alderick Hernán Rios Contreras <133692643+alderickrios98@users.noreply.github.com>
2024-09-26 17:05:31 -0700

    refactor: Update BcryptService to use bcrypt for password hashing and comparison
    This commit updates the BcryptService class in the `bcrypt.service.ts` file. The `hash` method now uses bcrypt to hash the password with a cost factor of 10. The `compare` method also uses bcrypt to compare a password with a hash and returns a boolean result. These changes improve the security of password handling in the NestJS application.

--- src/providers/bcrypt/services/bcrypt.service.ts
+++ src/providers/bcrypt/services/bcrypt.service.ts
@@ -1,12 +1,33 @@
+/** 
+ * @fileoverview The BcryptService class provides methods for hashing and comparing passwords using bcrypt in a
+NestJS application. */
 import { Injectable } from '@nestjs/common';
 import bcrypt from 'bcrypt';
 
 @Injectable()
 export class BcryptService {
+  /**
+   * The hash function in TypeScript uses bcrypt to hash a password with a cost factor of 10.
+   * @param {string} password - The `password` parameter is a string that represents the user's
+   * password that needs to be hashed for security purposes.
+   * @returns A Promise that resolves to a hashed version of the input password using bcrypt with a
+   * cost factor of 10.
+   */
   hash(password: string): Promise<string> {
     return bcrypt.hash(password, 10);
   }
 
+  /**
+   * The function compares a password with a hash using bcrypt and returns a Promise with a boolean
+   * result.
+   * @param {string} password - The `password` parameter is a string that represents the plain text
+   * password that a user enters when trying to log in to a system or application.
+   * @param {string} hash - The `hash` parameter is a string that represents the hashed version of a
+   * password. It is typically generated using a hashing algorithm like bcrypt to securely store and
+   * compare passwords without storing the actual password in plain text.
+   * @returns A Promise that resolves to a boolean value indicating whether the provided password
+   * matches the given hash.
+   */
   compare(password: string, hash: string): Promise<boolean> {
     return bcrypt.compare(password, hash);
   }
