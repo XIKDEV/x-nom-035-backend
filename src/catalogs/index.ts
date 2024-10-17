0caf364aeb25948f009219d8180a61cd2821d1cf
Alderick Hernán Rios Contreras <133692643+alderickrios98@users.noreply.github.com>
2024-10-16 17:13:07 -0700

    feat(sections): :sparkles: Add new catalog [sections] with their questions

--- src/catalogs/index.ts
+++ src/catalogs/index.ts
@@ -3,6 +3,7 @@ export * from './cities';
 export * from './modules';
 export * from './permissions';
 export * from './roles';
+export * from './sections';
 export * from './states';
 export * from './type-test';
 export * from './types-module';
